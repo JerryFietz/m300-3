06c62b3f56c7ee2b138ef91ed527279e43be3a59
JerryFietz <jerry.fietz@icloud.com>
2019-05-06 09:48:47 +0200

    lb3 docker compose

---
+++ LB3/docker/pihole/php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "dbuser";
+$password = "1234";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
+
+<p>The webserver too!! (duh)</p>
+
+</body>
+</html>
